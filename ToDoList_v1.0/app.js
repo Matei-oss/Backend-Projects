5805dc7a0c033f01d0535ad9333c1024c61f8a75
MATEIU <IMATEIU@steelcase.com>
2021-08-15 09:03:41 +0300

    added add new ToDo functionality

--- ToDoList_v1.0/app.js
+++ ToDoList_v1.0/app.js
@@ -2,18 +2,36 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const { render } = require("ejs");
 
 const app = express();
+
+var items = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     var today = new Date();
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("<h1>It's the weekend</h1>");
-    } else {
-        res.send("<h1>nope, not weekend</h1>");
-    }
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list.ejs", { kindOfDay: day, newListItem: items });
 });
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");

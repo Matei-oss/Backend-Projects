e8b8f2c364cb5b7bc75eb6758f192387201c97d5
MATEIU <IMATEIU@steelcase.com>
2021-08-15 11:18:52 +0300

    added new vies

--- ToDoList_v1.0/app.js
+++ ToDoList_v1.0/app.js
@@ -7,7 +7,7 @@ const { render } = require("ejs");
 const app = express();
 
 var items = ["Todo Example - 1", "Todo Example - 2"];
-
+var workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,14 +24,36 @@ app.get("/", function(req, res) {
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list.ejs", { kindOfDay: day, newListItem: items });
+    res.render("list.ejs", { listTitle: day, newListItem: items });
 });
 
 app.post("/", function(req, res) {
     var item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/")
+    }
+
     items.push(item);
     console.log(item);
-    res.redirect("/");
+})
+
+app.get("/work", function(req, res) {
+    res.render("list.ejs", { listTitle: "Work List", newListItem: workItems });
+})
+
+app.get("/about", function(req, res) {
+    res.render("about.ejs");
+})
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
 
 app.listen(3000, function() {

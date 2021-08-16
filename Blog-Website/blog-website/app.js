1410efb8e9ca5808223ac63190a20ed144db3fde
MATEIU <IMATEIU@steelcase.com>
2021-08-16 21:02:52 +0300

    added rendering for contact page

--- Blog-Website/blog-website/app.js
+++ Blog-Website/blog-website/app.js
@@ -29,6 +29,11 @@ app.get("/about", function(req, res) {
     });
 })
 
+app.get("/contact", function(req, res) {
+    res.render("contact.ejs", {
+        contactDescription: contactContent,
+    });
+})
 
 
 

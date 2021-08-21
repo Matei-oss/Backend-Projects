b2c81d0f596b18d658bc358e674457bb42b7ec7c
MATEIU <IMATEIU@steelcase.com>
2021-08-21 12:38:42 +0300

    todolist elements are retrieved from mongoDB

--- ToDoList_v2.0 + MongoDB/app.js
+++ ToDoList_v2.0 + MongoDB/app.js
@@ -3,21 +3,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { render } = require("ejs");
-const date = require(__dirname + "/date.js");
-
-const
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/ToDoListDB", { useNewUrlParser: true });
 
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully saved default items to db!")
+    }
+});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    let day = date.getDate();
-    res.render("list.ejs", { listTitle: day, newListItem: items });
+    Item.find({}, function(err, foundItems) {
+        console.log(foundItems);
+        res.render("list.ejs", { listTitle: "Today", newListItem: foundItems });
+    })
+
 });
 
 app.post("/", function(req, res) {

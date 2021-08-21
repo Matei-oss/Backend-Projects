248e85bed354b32521925263076f01332ca850f7
MATEIU <IMATEIU@steelcase.com>
2021-08-21 14:14:45 +0300

    redirect after item removal works

--- ToDoList_v2.0 + MongoDB/app.js
+++ ToDoList_v2.0 + MongoDB/app.js
@@ -111,15 +111,29 @@ app.post("/", function(req, res) {
 
 app.post("/delete", function(req, res) {
     const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
 
-    Item.findByIdAndRemove(checkedItemId, function(err) {
-        if (!err) {
-            console.log("Successfully deleted item!");
-            res.redirect("/");
-        }
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function(err) {
+            if (!err) {
+                console.log("Successfully deleted item!");
+                res.redirect("/");
+            }
 
-    });
-})
+        });
+    } else {
+        List.findOneAndUpdate({ name: listName }, {
+                $pull: { items: { _id: checkedItemId } }
+            },
+            function(err, foundList) {
+                if (!err) {
+                    res.redirect("/" + listName);
+                }
+            });
+    }
+
+
+});
 
 
 app.get("/about", function(req, res) {

7b75bf46605416eb0eb4dbfbc055b51eab507ab6
MATEIU <IMATEIU@steelcase.com>
2021-08-23 13:11:17 +0300

    added DELETE route

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -96,6 +96,16 @@ app.route("/articles/:articleTitle")
     });
 })
 
+.delete(function(req, res) {
+    Article.deleteOne({ title: req.params.articleTitle }, function(err) {
+        if (!err) {
+            res.send("Successfully deleted field!");
+        } else {
+            res.send(err);
+        }
+    })
+})
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");

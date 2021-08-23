2550612006bce421896e7083769461a22823427a
MATEIU <IMATEIU@steelcase.com>
2021-08-23 11:19:21 +0300

    defined mongoDB schema/ model

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -7,7 +7,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/WikiDB', { useNewUrlParser: true });
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchema = ({
     title: String,
@@ -15,15 +22,6 @@ const articleSchema = ({
 });
 
 const Article = mongoose.model("Article", articleSchema);
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-// app.use(express.static("public"));
-
-// //TODO
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");

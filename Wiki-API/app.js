9ada8b6d5dccd16c434c3b2c709c87a36cbce3be
MATEIU <IMATEIU@steelcase.com>
2021-08-23 11:33:47 +0300

    added GET route

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -23,6 +23,15 @@ const articleSchema = ({
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", function(req, res) {
+    Article.find({}, function(err, foundArticles) {
+        if (!err) { res.send(foundArticles); } else {
+            res.send(err);
+        }
+
+    })
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });

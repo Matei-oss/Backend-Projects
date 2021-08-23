e90c5264f1f89934489b2cae87995eeba52163e9
Matei-oss <imateiu@DEGL4J3Z2.emea.steelcase.net>
2021-08-23 07:16:35 +0300

    set up MongoDB connection

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -7,14 +7,23 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/WikiDB', { useNewUrlParser: true });
+
+const articleSchema = ({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-//TODO
+// //TODO
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");

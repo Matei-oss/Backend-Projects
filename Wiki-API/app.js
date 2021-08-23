85071713ed8578f450881155d8be8122ae8a3a02
MATEIU <IMATEIU@steelcase.com>
2021-08-23 12:05:02 +0300

    added chain route handlers

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -23,32 +23,41 @@ const articleSchema = ({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
-    Article.find({}, function(err, foundArticles) {
-        if (!err) { res.send(foundArticles); } else {
-            res.send(err);
-        }
+app.route("/articles")
+    .get(function(req, res) {
+        Article.find({}, function(err, foundArticles) {
+            if (!err) { res.send(foundArticles); } else {
+                res.send(err);
+            }
 
+        })
     })
-})
+    .post(function(req, res) {
+        console.log(req.body.title);
+        console.log(req.body.content);
 
-app.post("/articles", function(req, res) {
-    console.log(req.body.title);
-    console.log(req.body.content);
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
 
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        newArticle.save(function(err) {
+            if (!err) {
+                res.send("Succesfully added a new article!");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res) {
+        Article.deleteMany({}, function(err) {
+            if (!err) {
+                res.send("Successfully deleted all articles!")
+            } else {
+                res.send(err);
+            }
+        })
     });
-
-    newArticle.save(function(err) {
-        if (!err) {
-            res.send("Succesfully added a new article!");
-        } else {
-            res.send(err);
-        }
-    });
-})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");

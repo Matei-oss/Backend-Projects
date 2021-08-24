af3bf17118383f6378e75cbec5eaf06f20473da1
MATEIU <IMATEIU@steelcase.com>
2021-08-24 18:12:34 +0300

    added Google Strategy

--- Secrets/app.js
+++ Secrets/app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 // const encrypt = require("mongoose-encryption"); - MONGOOSE-ENCRYPTION
 // const md5 = require("md5");
 
@@ -46,6 +48,18 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 ///GET METHODS///
 app.get("/", function(req, res) {

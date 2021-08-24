c8eb3107b11d0aecd4c2690c20ffafdc831ae55a
MATEIU <IMATEIU@steelcase.com>
2021-08-24 10:19:17 +0300

    created functionality to add users

--- Secrets/app.js
+++ Secrets/app.js
@@ -6,12 +6,21 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true };)
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+///GET METHODS///
 app.get("/", function(req, res) {
     res.render("home.ejs");
 })
@@ -24,7 +33,22 @@ app.get("/register", function(req, res) {
     res.render("register.ejs");
 })
 
+///POST METHODS///
 
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("secrets.ejs")
+        }
+    });
+    console.log(newUser);
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.")

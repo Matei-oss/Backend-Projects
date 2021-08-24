91ad5f033a6252085f0c836c88231baf58faafcf
MATEIU <IMATEIU@steelcase.com>
2021-08-24 11:35:25 +0300

    added encryption plugin

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,15 +3,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+
+const secret = "x?$6Lp*U+sT=RVt?";
+
+userSchema.plugin(encrypt, { secret: secret });
 
 const User = new mongoose.model("User", userSchema);
 

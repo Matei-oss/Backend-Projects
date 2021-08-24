97ee1cb33b1eccf6636186f168b9b0daf4d40bf3
MATEIU <IMATEIU@steelcase.com>
2021-08-24 09:59:35 +0300

    added routes

--- Secrets/app.js
+++ Secrets/app.js
@@ -9,6 +9,19 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+})
+
+app.get("/login", function(req, res) {
+    res.render("login.ejs");
+})
+
+app.get("/register", function(req, res) {
+    res.render("register.ejs");
+})
+
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.")

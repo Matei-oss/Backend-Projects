608259dda979f83f7883346dba5c3ee4aa9f8b7d
MATEIU <IMATEIU@steelcase.com>
2021-08-24 18:19:38 +0300

    added new mongoose package

--- Secrets/app.js
+++ Secrets/app.js
@@ -52,7 +52,8 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets"
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, cb) {
         User.findOrCreate({ googleId: profile.id }, function(err, user) {

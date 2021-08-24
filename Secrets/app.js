ccee8b6d4fdf0eef12e5d9ba65a4d6e41e2b214e
MATEIU <IMATEIU@steelcase.com>
2021-08-24 10:05:41 +0300

    added connection to MongoDB

--- Secrets/app.js
+++ Secrets/app.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true };)
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");

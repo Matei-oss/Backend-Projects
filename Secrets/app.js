d8291491ec8aca56fcc31a745f7e848548347ffe
MATEIU <IMATEIU@steelcase.com>
2021-08-24 09:55:29 +0300

    added express ejs bodyparser packages

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,1 +1,15 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.")
+});

9a685e8106a3432201cb4d84a3cc98a663ab17cc
MATEIU <IMATEIU@steelcase.com>
2021-10-07 13:50:46 +0300

    added Footer component

--- keeper_app/src/components/App.jsx
+++ keeper_app/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
     return <div>
         <Header />
+        <Footer />
     </div>
 }
 
---
+++ keeper_app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer(){
+
+    const currentYear = new Date().getFullYear();
+
+    return <p>
+        Copyright Ⓒ {currentYear}
+    </p>
+}
+
+export default Footer;

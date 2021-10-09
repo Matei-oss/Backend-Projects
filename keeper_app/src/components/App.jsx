819e46903cd465fcfff0bd7867a20dc1f624ba38
MATEIU <IMATEIU@steelcase.com>
2021-10-09 10:16:03 +0300

    exported notes const/ used map() to iterate through array

--- keeper_app/src/components/App.jsx
+++ keeper_app/src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
 
 
+function createNotes(noteItem){
+return(<Note 
+    key={noteItem.key}
+    title={noteItem.title} 
+    content={noteItem.content} 
+
+    />)
+}
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(createNotes)}
         <Footer />
     </div>
 }
